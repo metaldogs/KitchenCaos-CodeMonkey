a3c024deac8e4dd18309d616f2ad5cd5c14d594d
Nicolas Castro <nicolas.castro@facens.br>
2024-02-05 23:16:18 -0300

    3horas37minutos

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,60 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+
+    private KitchenObject kitchenObject;
+    private bool testing = true;
+
+
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
+
     public void Interact()
     {
-        
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, spawnPoint).transform;
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;   
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+
+    public Transform GetRespawnPoint()
+    {
+        return spawnPoint;
     }
 
 
